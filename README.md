b6712148d8ef67c3088498cfe21fd6ee012959ac
jsavoie <julien.savoie@usainteanne.ca>
2016-01-11 10:56:52 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,6 @@
+Minimum free space
+=================
+
+Simple Python script for maintaining a file system at a minimum percentage of free space.
+Useful for pruning backup directories rather than deleting by age.
+Script will delete the oldest file in the referrenced directory.
---
+++ min-free-space.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+import os, sys
+
+def delete_oldest(directory):
+	os.chdir(directory)
+	files = filter(os.path.isfile, sorted(os.listdir(directory), key=os.path.getmtime))
+	if files:
+		print "Deleting file %s" % files[0]
+		os.remove(files[0])
+	else:
+		print "Nothing to delete"
+		sys.exit(1)
+
+def get_used(filesystem):
+	fs_stats = os.statvfs(filesystem)
+	return (1 - (float(fs_stats.f_bfree) / float(fs_stats.f_blocks))) * 100
+
+filesystem = "/home/media"
+directory = "/home/media/backups"
+desiredused = 98.0
+
+while get_used(filesystem) > desiredused:
+	delete_oldest(directory)
